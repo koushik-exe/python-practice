c9b2a8345b235a326e4fbc68681f2ef0d8da4277
koushik-exe <c.koushik2003@gmail.com>
2026-04-03 19:55:42 +0530

    Modified

--- koushik.py
+++ koushik.py
@@ -1,1 +1,8 @@
 print("Hello, World! Welcome to Python programming.")
+# This is a simple Python program that prints a welcome message to the console.
+# You can run this code in any Python environment to see the output.
+# You can also modify the message to display something else if you like.
+print("This is a practice file for learning Python.")
+# Feel free to experiment with different print statements and see how they work.
+# You can also add comments to your code to explain what it does.
+# Happy coding!
