2d676edd01708cdd05107af3ff18d1046bcb8c21
koushik-exe <c.koushik2003@gmail.com>
2026-04-06 18:12:55 +0530

    Data_types_has_been_Practiced

---
+++ Basics/Data_Types.py
@@ -0,0 +1,31 @@
+# Data Types Practice
+
+num = 10
+print(num, type(num))
+
+decimal = 10.5
+print(decimal, type(decimal))
+
+text = "Hello"
+print(text, type(text))
+
+flag1 = True
+print(flag1, type(flag1))
+
+flag2 = False
+print(flag2, type(flag2))
+
+my_list = [1, 2, 3]
+print(my_list, type(my_list))
+
+my_tuple = (1, 2, 3)
+print(my_tuple, type(my_tuple))
+
+my_set = {1, 2, 3}
+print(my_set, type(my_set))
+
+my_dict = {"name": "Koushik", "age": 22}
+print(my_dict, type(my_dict))
+
+nothing = None
+print(nothing, type(nothing))
