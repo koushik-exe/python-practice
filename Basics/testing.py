8e4a84188b3873572e4aea74617768e4faa4010b
koushik-exe <c.koushik2003@gmail.com>
2026-04-15 08:58:44 +0530

    Testing of the all types of LEGB

--- Basics/testing.py
+++ Basics/testing.py
@@ -1,2 +1,12 @@
-cities = ["Chennai", "Mumbai", "Delhi"]
-print(cities[3])  # Output: Chennai
+city = "Coimbatore"
+
+def company():
+    company_name = "GenZ"
+
+    def employee():
+        employee_name = "Koushik"
+        print(f"{employee_name} works in {company_name} in {city}")
+
+    employee()
+
+company()
