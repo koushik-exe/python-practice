316a4784984bc4cadd9aa9cf64c868504a388098
koushik-exe <c.koushik2003@gmail.com>
2026-04-15 09:00:05 +0530

    learned the bultin some function in python

---
+++ Basics/LEGB/builtin.py
@@ -0,0 +1,13 @@
+# B - Built in Scope
+# These are functions Python brings ready made
+# You never define them - they just work everywhere
+
+name = "Koushik"
+age = 23
+numbers = [10, 20, 30]
+
+print(name)
+print(type(name))
+print(type(age))
+print(str(age))
+print(len(numbers))
