d248c39891efcc0c45608079617612fbc194faf3
koushik-exe <c.koushik2003@gmail.com>
2026-05-22 14:08:39 +0530

    update the files in git hub

--- Basics/testing.py
+++ Basics/testing.py
@@ -1,4 +1,26 @@
-from math_tools import add, multiply
+from abc import ABC, abstractmethod
 
-print(add(5, 10))
-print(multiply(5, 10))
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+class Rectangle(Shape):
+    def  __init__ (self,length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        print(f"Total Area of Rectangle is: {self.length * self.width}")
+
+class Circle(Shape):
+    def __init__ (self, radius):
+        self.radius = radius
+
+    def area(self):
+        print(f"The Total Area of Circle is: {3.14*self.radius*self.radius}")
+
+R1 = Rectangle(10,10)
+R1.area()
+
+C1= Circle(30)
+C1.area()
